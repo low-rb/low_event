578d48894360397eadef9ac80aa1d191b19a68bd
maedi <maediprichard@gmail.com>
2026-05-22 00:32:37 +1000

    Trigger specific Status[code] and then general Status

--- lib/events/status_event.rb
+++ lib/events/status_event.rb
@@ -7,11 +7,18 @@ module Low
     class StatusEvent < Event
       attr_reader :status, :request
 
-      def initialize(status:, request:, action: :render)
-        super(key: status, action:)
+      def initialize(status:, request:, key: nil, action: :render)
+        super(key: key || status, action:)
 
         @status = status
         @request = request
+      end
+
+      # Status is a complex type that can be represented by both "Status" generically and "Status[:code]" specifically.
+      class << self
+        def trigger(status:, **kwargs)          
+          new(status:, **kwargs).trigger || new(status:, key: Status, **kwargs).trigger
+        end
       end
     end
   end
